65cf6e85cb7afc13745a9715bdb43e600d906103
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-06-19 16:18:29 +0700

    Update theme.blade.php

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -30,7 +30,14 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
                             <li class="nav-item">
-                                <a class="nav-link"></a>
+                                <span>
+                                    <div style="margin-top:-25px; margin-left:955px;position: absolute;">
+                                        <div id="google_translate_element"></div>
+                                    </div>
+                                    <div style="margin-top:-12px; margin-left:1070px;position: absolute;">
+                                        <i style="font-size:30px;" class="fas fa-language float-right"></i>
+                                    </div>
+                                </span>
                             </li>
                         </ul>
                     </div>
@@ -40,7 +47,6 @@
             <header class="py-5">
                 <div class="container px-5 pb-5">
                     <div class="row gx-5 align-items-center">
-                        <div id="google_translate_element"></div>
                         @yield('content')
                     </div>
                 </div>
@@ -72,15 +78,41 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="{{ asset('theme/js/scripts.js') }}"></script>
-        <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        
         <script type="text/javascript">
+            var comboGoogleTradutor = 'null'; //Varialvel global
+
             function googleTranslateElementInit() {
-                new google.translate.TranslateElement({
-                    pageLanguage: 'th', // หรือ 'en' แล้วแต่ภาษาเริ่มต้นของหน้า
-                    includedLanguages: 'th,en,zh-CN,ja,ko', // เลือกภาษาที่ต้องการให้แปล
-                    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-                }, 'google_translate_element');
+              new google.translate.TranslateElement({
+                pageLanguage: 'po',
+                // includedLanguages: 'th,en,zh-TW,ja,ko,es',
+                layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+              }, 'google_translate_element');
+
+              comboGoogleTradutor = document.getElementById("google_translate_element").querySelector(".goog-te-combo");
             }
-        </script>
+
+            function changeEvent(el) {
+              if (el.fireEvent) {
+                el.fireEvent('onchange');
+              } else {
+                var evObj = document.createEvent("HTMLEvents");
+
+                evObj.initEvent("change", false, true);
+                el.dispatchEvent(evObj);
+              }
+            }
+
+            function trocarIdioma(sigla) {
+              // console.log("trocarIdioma");
+              // console.log(sigla);
+              if (comboGoogleTradutor) {
+                comboGoogleTradutor.value = sigla;
+                changeEvent(comboGoogleTradutor); //Dispara a troca
+              }
+            }
+          </script>
+          <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        
     </body>
 </html>
